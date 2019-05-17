86c2d2b7f2e2e0ce137db5c0633a21a4bf02a82a
David Chen <chenwei2000121@163.com>
2019-05-17 12:00:40 -0700

    tmp

--- build-up/calc.py
+++ build-up/calc.py
@@ -1,30 +1,99 @@
+from classes import *
 
-class MathExpression(object):
-    def __init__(self):
-        self.firstElement = None
-        self.power = None
-        self.result = 0
+s = "12+(345-34)"
+s_len = 0
 
-class MathProduct (object):
-    def __init__(self):
-        
+__DEBUG_PAUSE = False
+__DEBUG_OUTPUT = False
 
-class MathSum(object):
-    def __init__(self):
-        self.contentString = ""
-        self.contentNumber = 0
-        self.next = None
-        self.origin = None
-        self.power = None
-        self.Children = None
+def convertMathString():
+    global s, s_len
+    print(s)
+    s_len = len(s)
+    print(s_len)
+    root = MathSum()
+    buildSum(root, 0, "", 0)
+    return root
 
-def convertMathString(s):
-    for char in s:
-        print(char)
-    exp = MathExpression()
-    return exp
 
-mathString = input().strip()
-exp = convertMathString(mathString)
-print(exp.result)
+def buildSum(obj, startLoc, pairChar, level):
+    global s, s_len
+    loc = startLoc
+    obj.pairChar = pairChar
+    while True:
+        if __DEBUG_OUTPUT:
+            print("level:", level, "summing", loc, s[loc])
+        if __DEBUG_PAUSE:
+            _ = input()
+
+        if loc > s_len - 1:
+            break
+        newProduct = MathProduct()
+        newProduct.origin = obj
+        obj.children.append(newProduct)
+        if __DEBUG_OUTPUT:
+            print("level:", level, "___add child", obj.children)
+        if s[loc] == "-":
+            newProduct.modifier = -1
+            loc += 1
+        if s[loc] == "+":
+            loc += 1
+        if __DEBUG_OUTPUT:
+            print("level:", level, "will_build_product")
+        loc = buildProduct(newProduct, loc, level + 1)
+        if s[loc] == ")":
+            if __DEBUG_OUTPUT:
+                print("level:", level, "End of Sum", "curloc:", loc)
+            return loc
+
+    return loc
+
+
+def buildProduct(obj, startLoc, level):
+    global s, s_len
+    loc = startLoc
+    while True:
+        if __DEBUG_OUTPUT:
+            print("level:", level, "producting", loc, s[loc])
+        if __DEBUG_PAUSE:
+            _ = input()
+        if loc >= s_len:
+            break
+        elif s[loc] == "+" or s[loc] == "-":
+            break
+        elif s[loc].isnumeric():
+            newElement = MathElement()
+            newElement.origin = obj
+            obj.children.append(newElement)
+            loc = buildElement(newElement, loc, level + 1)
+        elif s[loc] == "(":
+            newSum = MathSum()
+            newSum.origin = obj
+            obj.children.append(newSum)
+            loc = buildSum(newSum, loc + 1, pairChar = "(", level = level + 1)
+        elif s[loc] == ")":
+            if __DEBUG_OUTPUT:
+                print("level:", level, "end of sum", "curloc:", loc)
+            return loc
+
+    return loc
+
+
+def buildElement(obj, startLoc, level):
+    global s, s_len
+    loc = startLoc
+    if s[loc].isnumeric():
+        while s[loc].isnumeric():
+            loc += 1
+            if loc >= s_len:
+                break
+        obj.contentString = s[startLoc:loc]
+        obj.contentNumber = int(obj.contentString)
+
+    return loc
+
+
+# mathString = input().strip()
+expRoot = convertMathString()
+printExp(expRoot, 0)
 
--- build-up/classes.py
+++ build-up/classes.py
@@ -1,13 +1,49 @@
-class MathExpression(object):
+eID = 0
+
+class MathSum(object):
     def __init__(self):
-        self.firstElement = None
+        global eID
+        self.id = eID
+        eID += 1
+        self.pairStr = ""
+        self.children = []
+        self.power = None
         self.result = 0
+        self.origin = None
+
+class MathProduct(object):
+    def __init__(self):
+        global eID
+        self.id = eID
+        eID += 1
+        self.modifier = 1
+        self.children = []
+        self.origin = None
+        
 
 class MathElement(object):
     def __init__(self):
+        global eID
+        self.id = eID
+        eID += 1
         self.contentString = ""
         self.contentNumber = 0
-        self.next = None
         self.origin = None
-        self.Children = None
+        self.power = None
+        self.children = []
+
+def printExp(root, indent):
+    itemOrigin = root.origin
+    itemOriginID = -1
+    if root.origin != None:
+        itemOriginID = root.origin.id
+    if type(root) == MathSum:
+        print(' '*indent + "Sum (" + str(root.id) + "), type: " + root.pairStr + " origin: " + str(itemOriginID))
+    elif type(root) == MathProduct:
+        print(' '*indent + "Product (" + str(root.id) + "), modifier: " + str(root.modifier) + " origin: " + str(itemOriginID))
+    elif type(root) == MathElement:
+        print(' '*indent + "Element (" + str(root.id) + "): " + root.contentString + " origin: " + str(itemOriginID))
+    for child in root.children:
+        printExp(child, indent + 3)
+
 
